4e3c805f4a85a5787daf4c349f9837a8e61dc9b8
urbanix <urbanix@gmail.com>
2018-10-06 22:44:46 -0400

    Update index for redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { render } from 'react-dom'
+import * as React from 'react'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
 import { App } from './components/App_Redux';
 import './css/index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root') as HTMLElement
-);
+const store = createStore(rootReducer)
 
+render(
+     <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+)
 registerServiceWorker(); 
