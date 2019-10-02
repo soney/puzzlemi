0830a8cb134005454d9f1d3cb4e036031c138140
Steve Oney <swloney@gmail.com>
2019-10-02 12:09:29 -0400

    more bugfixes

---
+++ build/precache-manifest.baba6878890b4db8d0444490ee7e2c13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9debd96614ea79970fceda55ea4712af",
+    "url": "/pm_web/index.html"
+  },
+  {
+    "revision": "1d7037755cfafaeb9d3a",
+    "url": "/pm_web/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "95a7dcd9812f28deab79",
+    "url": "/pm_web/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "1d7037755cfafaeb9d3a",
+    "url": "/pm_web/static/js/2.785589d2.chunk.js"
+  },
+  {
+    "revision": "95a7dcd9812f28deab79",
+    "url": "/pm_web/static/js/main.0c441d69.chunk.js"
+  },
+  {
+    "revision": "d9ae9b6be105ee134b90",
+    "url": "/pm_web/static/js/runtime-main.e500a07a.js"
+  }
+]);
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -3,7 +3,7 @@ import { problems } from './problems';
 import { user } from './user';
 import { doc } from './sharedb';
 import { userData } from './userData';
-​
+
 export default combineReducers({
     doc,
     problems,
