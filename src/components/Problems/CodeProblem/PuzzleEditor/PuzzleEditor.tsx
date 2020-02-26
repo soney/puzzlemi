af0d9b9f7fa1017595290adf2ec833aa6f6c54ae
April <aprilww@umich.edu>
2020-02-26 09:35:27 -0500

    🚑 attempt to fix the status of default instructor's test

--- src/components/Problems/CodeProblem/PuzzleEditor/PuzzleEditor.tsx
+++ src/components/Problems/CodeProblem/PuzzleEditor/PuzzleEditor.tsx
@@ -38,6 +38,7 @@ const PuzzleEditor = ({ userSolution, graphicsRef, myuid, myemail, allTests, pro
 
     const doInitTestStatus = () => {
         if (currentTest.status === CodeTestStatus.UNVERIFIED) return;
+        if (currentTest.author === null) return;
         if (isAdmin) return;
         const newStatus = CodeTestStatus.UNVERIFIED;
         dispatch(changeTestStatus(problem.id, currentTest, newStatus));
@@ -45,6 +46,7 @@ const PuzzleEditor = ({ userSolution, graphicsRef, myuid, myemail, allTests, pro
 
 
     const doChangeTestStatus = () => {
+        if (currentTest.author === null) return;
         const newStatus = currentTest.status === CodeTestStatus.VERIFIED ? CodeTestStatus.VERIFICATION_FAILED : CodeTestStatus.VERIFIED;
         dispatch(changeTestStatus(problem.id, currentTest, newStatus))
         analytics.logEvent("verify_test", {problemID: problem.id, channel, user: myemail, test: JSON.stringify(currentTest), status: newStatus});
